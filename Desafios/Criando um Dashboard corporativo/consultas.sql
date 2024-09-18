340bcd0471a08808233ac808a45ef2b5d2a6b472
NatashaB-randao <natashabrandao57@hotmailcom>
2024-09-18 17:09:19 -0300

    Atualização

--- Desafios/Criando um Dashboard corporativo/consultas.sql
+++ Desafios/Criando um Dashboard corporativo/consultas.sql
@@ -4,98 +4,95 @@
 SELECT * FROM funcionario;
 
 -- Contar dependentes por funcionário
-SELECT Ssn AS CPF, COUNT(Essn) AS Total_dependentes 
+SELECT CPF_Funcionario AS CPF, COUNT(Nome_Dependente) AS Total_Dependentes
 FROM funcionario f
-JOIN dependente d ON f.Ssn = d.Essn
-GROUP BY Ssn;
+JOIN dependente d ON f.CPF = d.CPF_Funcionario
+GROUP BY CPF_Funcionario;
 
 -- Selecionar todos os dependentes
 SELECT * FROM dependente;
 
 -- Obter a data de nascimento e endereço de John B. Smith
-SELECT Bdate AS Data_de_Nascimento, Endereco 
+SELECT Data_Nascimento, Endereco 
 FROM funcionario
-WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
+WHERE Primeiro_Nome = 'John' AND Inicial_Nome_Meio = 'B' AND Sobrenome = 'Smith';
+
 
 -- Selecionar departamento 'Pesquisa'
-SELECT * FROM departamento 
-WHERE Dname = 'Pesquisa';
+SELECT * FROM departamento WHERE Nome_Departamento = 'Pesquisa';
+
 
 -- Obter informações dos funcionários do departamento 'Pesquisa'
-SELECT Fname AS Nome, Lname AS Sobrenome, Endereco 
+SELECT Primeiro_Nome, Sobrenome, Endereco
 FROM funcionario
-JOIN departamento ON funcionario.Dno = departamento.Dnumero
-WHERE departamento.Dname = 'Pesquisa';
+JOIN departamento ON funcionario.Numero_Departamento = departamento.Numero_Departamento
+WHERE Nome_Departamento = 'Pesquisa';
 
 -- Selecionar todos os projetos
 SELECT * FROM projeto;
 
--- Recuperar informações dos departamentos localizados em Stafford
-SELECT Dname AS Departamento, Mgr_ssn AS Gerente 
+-- Recuperando informações dos departamentos localizados em Stafford
+SELECT Nome_Departamento AS Departamento, CPF_Gerente AS Gerente 
 FROM departamento d
-JOIN localizacoes_departamento l ON d.Dnumero = l.Dnumero
-WHERE l.Localizacao = 'Stafford';
+JOIN localizacoes_departamento l ON d.Numero_Departamento = l.Numero_Departamento
+WHERE l.Localizacao_Departamento = 'Stafford';
 
--- Concatenar nomes de gerente
-SELECT Dname AS Departamento, CONCAT(Fname, ' ', Lname) AS Gerente 
+-- Concatenando nomes de gerente
+SELECT Nome_Departamento AS Departamento, CONCAT(f.Primeiro_Nome, ' ', f.Sobrenome) AS Gerente 
 FROM departamento d
-JOIN localizacoes_departamento l ON d.Dnumero = l.Dnumero
-JOIN funcionario f ON d.Mgr_ssn = f.Ssn;
+JOIN localizacoes_departamento l ON d.Numero_Departamento = l.Numero_Departamento
+JOIN funcionario f ON d.CPF_Gerente = f.CPF;
 
--- Recuperar projetos localizados em Stafford
+-- Recuperando projetos localizados em Stafford
 SELECT * FROM projeto
-JOIN departamento ON projeto.Dnum = departamento.Dnumero
-WHERE projeto.Plocalizacao = 'Stafford';
+JOIN departamento ON projeto.Numero_Departamento = departamento.Numero_Departamento
+WHERE Localizacao_Projeto = 'Stafford';
 
--- Recuperar informações de departamentos e projetos localizados em Stafford
-SELECT projeto.Pnumero AS Número_do_Projeto, departamento.Dnum AS Número_do_Departamento, funcionario.Lname AS Sobrenome_Gerente, funcionario.Endereco, funcionario.Bdate AS Data_de_Nascimento
+-- Recuperando informações de departamentos e projetos localizados em Stafford
+SELECT projeto.Numero_Projeto, departamento.Numero_Departamento, f.Sobrenome, f.Endereco, f.Data_Nascimento
 FROM projeto
-JOIN departamento ON projeto.Dnum = departamento.Dnumero
-JOIN funcionario ON departamento.Mgr_ssn = funcionario.Ssn
-WHERE projeto.Plocalizacao = 'Stafford';
+JOIN departamento ON projeto.Numero_Departamento = departamento.Numero_Departamento
+JOIN funcionario f ON departamento.CPF_Gerente = f.CPF
+WHERE projeto.Localizacao_Projeto = 'Stafford';
 
 -- Selecionar funcionários de certos departamentos
-SELECT * FROM funcionario 
-WHERE Dno IN (3, 6, 9);
+SELECT * FROM funcionario WHERE Numero_Departamento IN (3, 6, 9);
 
 -- Operadores lógicos
-SELECT Bdate AS Data_de_Nascimento, Endereco 
+SELECT Data_Nascimento, Endereco
 FROM funcionario
-WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
+WHERE Primeiro_Nome = 'John' AND Inicial_Nome_Meio = 'B' AND Sobrenome = 'Smith';
 
-SELECT Fname AS Nome, Lname AS Sobrenome, Endereco 
+SELECT Primeiro_Nome, Sobrenome, Endereco
 FROM funcionario
-JOIN departamento ON funcionario.Dno = departamento.Dnumero
-WHERE departamento.Dname = 'Pesquisa';
+JOIN departamento ON funcionario.Numero_Departamento = departamento.Numero_Departamento
+WHERE Nome_Departamento = 'Pesquisa';
 
 -- Recolhendo o valor do INSS
-SELECT Fname AS Nome, Lname AS Sobrenome, Salario, Salario * 0.011 AS INSS 
-FROM funcionario;
-
-SELECT Fname AS Nome, Lname AS Sobrenome, Salario, ROUND(Salario * 0.011, 2) AS INSS 
-FROM funcionario;
+SELECT Primeiro_Nome, Sobrenome, Salario, Salario * 0.011 AS INSS FROM funcionario;
+SELECT Primeiro_Nome, Sobrenome, Salario, ROUND(Salario * 0.011, 2) AS INSS FROM funcionario;
 
 -- Definindo aumento de salário para gerentes associados ao ProdutoX
-SELECT f.Fname AS Nome, f.Lname AS Sobrenome, 1.1 * f.Salario AS Salario_aumentado 
+SELECT f.Primeiro_Nome, f.Sobrenome, 1.1 * f.Salario AS Salario_Aumentado 
 FROM funcionario f
-JOIN trabalha_em t ON f.Ssn = t.Essn
-JOIN projeto p ON t.Pno = p.Pnumero
-WHERE p.Pname = 'ProdutoX';
+JOIN trabalha_em t ON f.CPF = t.CPF_Funcionario
+JOIN projeto p ON t.Numero_Projeto = p.Numero_Projeto
+WHERE p.Nome_Projeto = 'ProdutoX';
 
 -- Concatenando e fornecendo alias
-SELECT Dname AS Departamento, CONCAT(Fname, ' ', Lname) AS Gerente 
+SELECT Nome_Departamento AS Departamento, CONCAT(f.Primeiro_Nome, ' ', f.Sobrenome) AS Gerente 
 FROM departamento d
-JOIN localizacoes_departamento l ON d.Dnumero = l.Dnumero
-JOIN funcionario f ON d.Mgr_ssn = f.Ssn;
+JOIN localizacoes_departamento l ON d.Numero_Departamento = l.Numero_Departamento
+JOIN funcionario f ON d.CPF_Gerente = f.CPF;
 
--- Recuperar dados dos funcionários que trabalham no departamento de Pesquisa
-SELECT Fname AS Nome, Lname AS Sobrenome, Endereco 
+-- Recuperando dados dos funcionários que trabalham no departamento de Pesquisa
+SELECT Primeiro_Nome, Sobrenome, Endereco 
 FROM funcionario
-JOIN departamento ON funcionario.Dno = departamento.Dnumero
-WHERE departamento.Dname = 'Pesquisa';
+JOIN departamento ON funcionario.Numero_Departamento = departamento.Numero_Departamento
+WHERE Nome_Departamento = 'Pesquisa';
 
 -- Definindo alias para legibilidade da consulta
-SELECT f.Fname AS Nome, f.Lname AS Sobrenome, f.Endereco 
+SELECT f.Primeiro_Nome, f.Sobrenome, f.Endereco 
 FROM funcionario f
-JOIN departamento d ON d.Dnumero = f.Dno
-WHERE d.Dname = 'Pesquisa';
+JOIN departamento d ON d.Numero_Departamento = f.Numero_Departamento
+WHERE d.Nome_Departamento = 'Pesquisa';
